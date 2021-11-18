4aeeedb3a72f4abd83a27e7f5677692a73049e93
Leblanc <leblanc@cas-web-lp001.phy.ohio.edu>
2021-11-18 17:18:40 -0500

    Find Certs by ID prototype

--- src/main/java/edu/ohio/inpp/acceleratorOperators/CertificationRepository.java
+++ src/main/java/edu/ohio/inpp/acceleratorOperators/CertificationRepository.java
@@ -1,7 +1,9 @@
 package edu.ohio.inpp.acceleratorOperators;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface CertificationRepository extends CrudRepository<Certification, Integer> {
-    
+    Optional<Certification> findById(Integer id);
 }
